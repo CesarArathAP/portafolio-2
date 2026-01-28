0a90ea5e13c80b8f1770d4319f49d760212be1ba
CesarArathAP <cesararath1976@gmail.com>
2026-01-28 14:22:45 -0600

    se comento la linea de astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+// https://astro.build/config configuracion para el despliegue en github pages
 export default defineConfig({
 	site: 'https://cesararathap.github.io',
 	base: '/portafolio-2',
